46b555659bfabf7203ce8ff55b9a5ee58b837e6e
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-08-08 15:20:17 -0400

    changes

--- insertlogin.php
+++ insertlogin.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-  <?php
+        <?php
         $con = mysql_connect("localhost", "kay", "dtycts16");
         if (!$con) {
             die("Connection failed:" . mysqli_connect_error());
@@ -15,30 +15,36 @@
         $userpassword = $_POST['userpassword'];
 
 
-        if(isset($_POST['submit']))
-        {
-            $con =  mysql_connect("localhost","kay","dtycts16");
-                    if(!$con){
-                        die("Cannot connect :" .sql_error());
-                        
-                    }
-                       mysql_select_db("kay", $con);
-            $sql = "select * from CUSTOMER where email=='$email' and  userpassword ='$userpassword')";
-            $result = mysqli_query($con, $sql);
-            if (!$row = mysqli_fetch_array($result))
-                    {
-                
-              
-           
-                
-            } else {
-                   echo "Login successful";
-                   echo $row['email'];
-            
+        // echo $email . $userpassword;
+
+        if (isset($_POST['submit'])) {
+            //  echo "something submitted.";
+
+            $con = mysqli_connect("localhost", "kay", "dtycts16", "kay");
+            if (!$con) {
+                echo "something wrong with connection.";
+                die("Cannot connect :" . sql_error());
             }
+
+            $sql = "select * from CUSTOMER where email=\"$email\"";
+            $result = mysqli_query($con, $sql);
+            //echo "query executed: " . $sql . " " . mysqli_num_rows($result) . " rows";
+
+            if (mysqli_num_rows($result) != 1) {
+                echo "no data found";
+            } else {
+                echo "hehhehehehehe";
+            }
+            if (mysqli_fetch_array($result['userpassword']) == $userpassword) {
+
+                echo "Login successful";
+            } else {
+
+                echo $result['email'];
+            }
+        } else {
+            echo "nothing submitted";
         }
-           
-        
         ?>
     </body>
 </html> 
