488c8e18f39c345d64badbe72237f873070767b7
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-08-08 14:29:43 -0400

    changes

--- insertlogin.php
+++ insertlogin.php
@@ -5,50 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-      <?php
-      include'dbhanler.php';
-if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['email']))
-{
-     ?>
- 
-     <h1>Member Area</h1>
-     <p>Thanks for logging in! You are <code><?=$_SESSION['email']?></code> and your email address is <code><?=$_SESSION['email']?></code>.</p>
-      
-     <?php
-}
-elseif(!empty($_POST['email']) && !empty($_POST['userpassword']))
-{
-    $email = mysql_real_escape_string($_POST['email']);
-    $userpassword = md5(mysql_real_escape_string($_POST['userpassword']));
-     
-    $checklogin = mysql_query("SELECT * FROM CUSTOMER WHERE email = '".$email."' AND userpassword = '".$userpassword."'");
-     
-    if(mysql_num_rows($checklogin) == 1)
-    {
-        $row = mysql_fetch_array($checklogin);
-        $email = $row['email'];
-         
-        $_SESSION['email'] = $email;
-   
-        $_SESSION['LoggedIn'] = 1;
-         
-        echo "<h1>Success</h1>";
-        echo "<p>We are now redirecting you to the member area.</p>";
-        echo "<meta http-equiv='refresh' content='=2;index.php' />";
-    }
-    else
-    {
-        echo "<h1>Error</h1>";
-        echo "<p>Sorry, your account could not be found. Please <a href=\"index.php\">click here to try again</a>.</p>";
-    }
-}
-else
-{
-    echo'';
-}
-    ?>
-
-        <?php
+  <?php
         $con = mysql_connect("localhost", "kay", "dtycts16");
         if (!$con) {
             die("Connection failed:" . mysqli_connect_error());
@@ -58,7 +15,7 @@ else
         $userpassword = $_POST['userpassword'];
 
 
-        if(isset($_POST['Login']))
+        if(isset($_POST['submit']))
         {
             $con =  mysql_connect("localhost","kay","dtycts16");
                     if(!$con){
@@ -68,15 +25,19 @@ else
                        mysql_select_db("kay", $con);
             $sql = "select * from CUSTOMER where email=='$email' and  userpassword ='$userpassword')";
             $result = mysqli_query($con, $sql);
-            if (!$row = mysqli_fetch_assoc($result)) {
-                echo "Login information incorrect";
+            if (!$row = mysqli_fetch_array($result))
+                    {
+                
+              
+           
                 
             } else {
-                echo "Login successful";
+                   echo "Login successful";
+                   echo $row['email'];
             
             }
         }
-            header("Location:inhome.php");
+           
         
         ?>
     </body>
