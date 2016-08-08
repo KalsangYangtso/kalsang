85b4b042b946df2f9fa445f663f3bde05073e927
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-08-07 23:34:49 -0400

    changes

--- insertlogin.php
+++ insertlogin.php
@@ -5,28 +5,48 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-          <div class="navi">
+        <div class="navi">
             <a href="home.php">Home</a>
             <a href="registration.php">Registration</a>
             <a href="login.php">Login</a>
             <a href="cart.php">Cart</a>
-            
+
         </div>
-   
-<?php
-        if(isset($_POST['submit']))
-        {
-            $con =  mysql_connect("localhost","kay","dtycts16");
-                    if(!$con){
-                        die("Cannot connect :" .sql_error());
-                        
-                    }
-                    mysql_select_db("kay", $con);
-                    $sql= "INSERT INTO CUSTOMER (email, userpassword) VALUES ('$_POST[email]', '$_POST[userpassword]')";
-                    mysql_query($sql, $con);
-                    mysql_close($con);
+
+        <?php
+        session_start();
+              if (isset($_POST['Login'])) {
+      
+        include_once ('db.php');
+
+  
+
+            $email = strip_tags($POST_['email']);
+            $userpassword = strip_tags($POST_['userpassword']);
+
+            $email = stripslashes($email);
+            $userpassword = stripslashes($userpassword);
+
+            $email = mysqli_real_escape_string($kay, $email);
+            $userpassword = mysqli_real_escape_string($kay, $userpassword);
+            
+            $userpassword = md5($userpassword);
+            $sql = "SELECT * FROM CUSTOMER where email ='$email' LIMIT 1";
+            $query = mysqli_query($kay, $sql);
+            $row = mysqli_fetch_array($query);
+            $db_userpassword = $row['userpassword'];
+
+            if ($userpassword == $kay_userpassword) {
+                $_session['email'] = $email;
+          
+                header("Location: home.php");
+            } 
+            else {
+                echo "Invalid Information";
+            }
         }
+       
         ?>
- </body>
+    </body>
 </html> 
 
